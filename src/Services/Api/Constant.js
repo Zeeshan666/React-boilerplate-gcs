96cdbae1fd51f2f7fd319dad921fd61772ada8f5
zeeshan666 <zeeshanmanzoor.zm@gmail.com>
2024-08-28 14:45:15 +0500

    minor fixes

--- src/Services/Api/Constant.js
+++ src/Services/Api/Constant.js
@@ -1,3 +1,1 @@
-//export const BASE_URL ='http://3.138.240.20:3006/api';
-//export const BASE_URL = "http://localhost:3001/api";
-export const BASE_URL ='http://3.138.240.20:8082/api';
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
